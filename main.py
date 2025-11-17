567c8a6b15bad1a0f1e35f5a0b37a1075bce7bf1
nithzhizn-cpu <nithzhizn@gmail.com>
2025-11-17 22:19:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from database import Base, engine
 import routes_messages
 import routes_calls
+import routes_users
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.include_router(routes_users.router)
 app.include_router(routes_messages.router)
 app.include_router(routes_calls.router)
 
