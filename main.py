ebdb69802d4076fc8b7b29536eed6f3178559c66
nithzhizn-cpu <nithzhizn@gmail.com>
2025-11-17 22:23:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from fastapi import FastAPI
 from database import Base, engine
-import routes_messages
-import routes_calls
-import routes_users
 
+# Імпорт роутерів
+from routes.users import router as users_router
+from routes.messages import router as messages_router
+from routes.calls import router as calls_router
+
+# Створення таблиць
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(title="SpySignal Backend")
 
-app.include_router(routes_users.router)
-app.include_router(routes_messages.router)
-app.include_router(routes_calls.router)
+# Підключення роутів
+app.include_router(users_router, prefix="/api")
+app.include_router(messages_router, prefix="/api")
+app.include_router(calls_router, prefix="/api")
 
 @app.get("/health")
 def health():
